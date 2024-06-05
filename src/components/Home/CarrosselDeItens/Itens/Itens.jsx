fa23a80bc1a70f57d482e77d1a89e092be2b73b2
Willian Oliveira <williancontato67@gmail.com>
2024-06-05 10:19:14 -0300

    Finalização do icone de avaliação do carrossel de filmes

--- src/components/Home/CarrosselDeItens/Itens/Itens.jsx
+++ src/components/Home/CarrosselDeItens/Itens/Itens.jsx
@@ -3,9 +3,20 @@ import style from './style.module.scss'
 import { Link } from 'react-router-dom';
 
 const Itens = ({ dado }) => {
+  const nota = Math.floor(dado.vote_average * 10)
+  let corNota;
+  if(nota <= 50) {
+    corNota = '#f13423'
+  } else if(nota > 50 && nota < 70) {
+    corNota = '#f1c423'
+  } else {
+    corNota = '#61e034'
+  }
+
   return (
     <Link to={`/${dado && dado.media_type}/${dado.id}`} className={style.itemConteiner}>
       <div>
+        <span className={style.itemNota} style={{backgroundColor: corNota}}>{nota}</span>
         <img
           src={`https://image.tmdb.org/t/p/w500/${dado.poster_path}`}
           alt={dado.title}

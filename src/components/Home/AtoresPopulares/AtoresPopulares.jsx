ca9455ff3a203fc19955b6b2d4c7c72093cafb19
Willian Oliveira <williancontato67@gmail.com>
2024-06-10 10:41:37 -0300

    Ajustes nos estilos, adicionando o maxWidth para telas maiorez

--- src/components/Home/AtoresPopulares/AtoresPopulares.jsx
+++ src/components/Home/AtoresPopulares/AtoresPopulares.jsx
@@ -3,6 +3,7 @@ import useFetch from '../../../hooks/useFetch'
 import Titulo from '../Titulo/Titulo'
 import { GET_ATORES_POPULARES } from '../../../api/api'
 import CarrosselAtores from './CarrosselAtores/CarrosselAtores'
+import style from './style.module.scss'
 
 const AtoresPopulares = () => {
   const {loading, requisicao, dados} = useFetch()
@@ -13,7 +14,7 @@ const AtoresPopulares = () => {
   }, [requisicao])
 
   return (
-    <section>
+    <section className={style.conteiner}>
       <Titulo>Atores populares</Titulo>
       {dados && <CarrosselAtores dados={dados} loading={loading}/>}
     </section>
